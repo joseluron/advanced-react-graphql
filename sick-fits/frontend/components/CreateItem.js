c3bdc2290d592a0fcfa035b335af9e9feede614e
jlcoalla_tribalyte <joseluron@gmail.com>
2019-06-06 14:55:15 +0200

    Implemented the upload of images section

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -35,10 +35,30 @@ class CreateItem extends React.Component {
         price: 123
     }
 
-    handleChange = (e) => {
+    handleChange = e => {
         const { name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name]: val });
+    }
+
+    uploadFile = async e => {
+        console.log('Uploading file');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/dzimmpuvy/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+
+        });
     }
     
     render() {
@@ -59,6 +79,12 @@ class CreateItem extends React.Component {
                     }}>
                         <Error error={error}/>
                         <fieldset disabled={loading} aria-busy={loading} >
+                            <label htmlFor="file">
+                                Image
+                                <input type="file" id="dile" name="dile" placeholder="Upload an image" required onChange={this.uploadFile} />
+                                {this.state.image && <img src={this.state.image} alt="Upload preview" />}
+                            </label>
+                            
                             <label htmlFor="title">
                                 Title
                                 <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handleChange} />
